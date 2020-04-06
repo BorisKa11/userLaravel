4e657cd7fb54a9ad6c354044d4e63f139547b823
BorisKa11 <bkazakov@mail.ru>
2020-04-06 22:46:31 +0200

    small upd

--- app/Helpers.php
+++ app/Helpers.php
@@ -1,21 +1,4 @@
 <?php
-function num2word($num, $words) {
-    $num = $num % 100;
-    if ($num > 19) {
-        $num = $num % 10;
-    }
-    switch ($num) {
-        case 1: {
-            return($words[0]);
-        }
-        case 2: case 3: case 4: {
-            return($words[1]);
-        }
-        default: {
-            return($words[2]);
-        }
-    }
-}
 function toDigits($n) {
 	return str_replace(['+7',' ','(',')','-'], '', $n);
 }
